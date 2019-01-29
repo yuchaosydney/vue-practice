4c14e9b9e98e088cdce308d118ae04110977df1a
yuchao <yuchao.sydneyuni@gmail.com>
2019-01-30 09:10:48 +1100

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const indexHtml = (() => {
+  return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8');
+})();
+
+app.use('/dist', express.static(path.resolve(__dirname, './dist')));
+
+app.get('*', (req, res) => {
+  res.write(indexHtml);
+  res.end();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+const app = new Vue({
+  data: {
+    hello: 'Hi there'
+  }
+})
+
+export { app };
