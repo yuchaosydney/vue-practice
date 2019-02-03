cb34b2efce19afbea4f373b16bfcc38a588bce2a
yuchao wu <yuchao.wu@xakiatech.com>
2019-02-03 13:27:20 +1100

    WIP

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,19 +1,37 @@
-const path = require('path');
+const path = require('path')
 
 const config = {
   entry: {
     app: path.resolve(__dirname, '../src/client-entry.js')
   },
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.js'
-    }
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /(\.js$)|(\.vue$)/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          css: 'css-loader',
+          'scss': 'css-loader|sass-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
     filename: 'assets/js/[name].js'
   }
-};
+}
 
-module.exports = config;
+module.exports = config
--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import Vue from 'vue';
+import Vue from 'vue'
+import AppLayout from './theme/Layout.vue'
 
 const app = new Vue({
-  data: {
-    hello: 'Hi there'
-  }
+  ...AppLayout
 })
 
-export { app };
+export { app }
