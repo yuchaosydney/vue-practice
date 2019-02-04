1bc7d3b5e07a95dedae8aafbf4b97f8869751067
yuchao wu <yuchao.wu@xakiatech.com>
2019-02-05 08:55:45 +1100

    WIP

--- src/theme/Post.vue
+++ src/theme/Post.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="card">
     <div class="card-content">
-      <h3>{{ post.title }}</h3>
-      {{ post.content }}
+      <slot name="title"></slot> 
+      <slot name="content"></slot> 
     </div>
     <footer class="card-footer">
-      <a class="card-footer-item" :href="post.link" target="_blank">Read More</a>
+      <a class="card-footer-item" :href="link" target="_blank">Read More</a>
     </footer>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['post']
+  props: ['link']
 }
 </script>
+
+<style scoped>
+  .card {
+    padding-bottom: 40px;
+    height: 100%;
+  }
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    left: 0;
+  }
+</style>
+
 
